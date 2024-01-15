a625cc1cef8753440e29d27fe670042751a39b4d
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-01-15 20:03:52 +0530

    Update account.py

--- helper/account.py
+++ helper/account.py
@@ -9,3 +9,16 @@ response = seedr.authorize()
 
 token = seedr.token
 account = Seedr(token=token)
+
+
+def clean():
+    storage = account.listContents()
+
+    for item_type in ['folders', 'files', 'torrents']:
+        for item in storage.get(item_type, []):
+            if item_type == 'folders':
+                account.deleteFolder(item['id'])
+            elif item_type == 'files':
+                account.deleteFile(item['id'])
+            else:
+                account.deleteTorrent(item['id'])
